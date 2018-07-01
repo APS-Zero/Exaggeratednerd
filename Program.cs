28259b08fc4508b5d9168e8f3df9cfb7e897ffd1
APS-Zero <33553539+APS-Zero@users.noreply.github.com>
2018-07-01 20:29:59 +0300

    made a dotnet project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exaggeratednerd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
