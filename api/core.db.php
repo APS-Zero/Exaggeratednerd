c60ae2c69053e6538f1f5d020bec3a6ad3f5a1e3
Owen Thompson <exaggeratednerd2004@gmail.com>
2018-07-02 15:31:37 +0100

    Update database manager script

--- api/core.db.php
+++ api/core.db.php
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 <?php
 
 $DB_HOST = "localhost";
 $DB_USERNAME = "root";
-$DB_PASSWORD = "478nscmonvrolazfhop8zhg9oj;qhebILH(GFOS*hg9)";
+$DB_PASSWORD = "b48786ad5e511050b722f1deb9720bd6963ce373a3840016";
 
 function sql_connect($dbname) {
   if ($dbname) return mysqli_connect($DB_HOST, $DB_USERNAME, $DB_PASSWORD, $dbname);
@@ -73,79 +72,3 @@ function sql_insert($connection, $table, $fname, $values, $dbname) {
 }
 
 ?>
-=======
-<?php
-
-$DB_HOST = "localhost";
-$DB_USERNAME = "root";
-$DB_PASSWORD = "478nscmonvrolazfhop8zhg9oj;qhebILH(GFOS*hg9)";
-
-function sql_connect($dbname) {
-  if ($dbname) return mysqli_connect($DB_HOST, $DB_USERNAME, $DB_PASSWORD, $dbname);
-  else return mysqli_connect($DB_HOST, $DB_USERNAME, $DB_PASSWORD);
-}
-
-function sql_create_db($dbname) {
-  $sql_query = "CREATE TABLE IF NOT EXISTS " . $dbname;
-  $conn = mysqli_connect($DB_HOST, $DB_USERNAME, $DB_PASSWORD);
-  mysqli_query($conn, $sql_query);
-}
-
-function sql_create_table($connection, $dbname, $tablename, $elements) {
-  $sql_query = "CREATE TABLE IF NOT EXISTS " . $tablename . "(" . $elements . ")";
-  $cif = false;
-  if (!$connection) {
-    $cif = true;
-    $connection = sql_connect($dbname);
-  }
-  mysqli_query($connection, $sql_query);
-  if ($cif) mysqli_close($connection);
-}
-
-function sql_make_query($connection, $query, $fname, $dbname) {
-  $cif = false;
-  if (!$connection) {
-    $cif = true;
-    $connection = sql_connect($dbname);
-  }
-  $result = mysqli_query($connection, $sql_query);
-  $fname($result);
-  if ($cif) mysqli_close($connection);
-}
-
-function sql_select($connection, $table, $fname, $where, $dbname) {
-  $sql_query = "SELECT * FROM " . $table;
-  if ($where) $sql_query = $sql_query . " WHERE " . $where;
-  $cif = false;
-  if (!$connection) {
-    $cif = true;
-    $connection = sql_connect($dbname);
-  }
-  $result = mysqli_query($connection, $sql_query);
-  $fname($result);
-  if ($cif) mysqli_close($connection);
-}
-
-function sql_insert($connection, $table, $fname, $values, $dbname) {
-  $sql_query = "INSERT INTO " . $table . " (";
-  $start = true;
-  foreach ($values as $name) {
-    if ($start) {
-      $sql_query = $sql_query . ;
-      $start = false;
-    }
-    $sql_query = $sql_query . ",";
-  }
-  $sql_query = $sql_query . ")";
-  $cif = false;
-  if (!$connection) {
-    $cif = true;
-    $connection = sql_connect($dbname);
-  }
-  $result = mysqli_query($connection, $sql_query);
-  $fname($result);
-  if ($cif) mysqli_close($connection);
-}
-
-?>
->>>>>>> 23a934e94c17352458f8b5a90c6a88ebfe8b1d45
